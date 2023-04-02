1cf759eb8c58cd2aadf5f102e80c51e9585c332d
Dirck Hecking <justadirck@gmail.com>
2023-04-02 01:23:01 -0400

    docker compose mysql8

--- mysql/sql/01-create-user.sql
+++
@@ -1,5 +0,0 @@
-create user 'ecommerceapp'@'%' identified by 'ecommerceapp';
-
-grant all privileges on *.* to 'ecommerceapp'@'%';
-
-alter user 'ecommerceapp'@'%' identified with mysql_native_password by 'ecommerceapp';
